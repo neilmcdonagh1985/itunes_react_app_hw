14dc2a18477a84fe326ef1099befea8a129f3af0
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2019-10-29 20:11:25 +0000

    api data sourced

---
+++ src/components/SongList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SongList extends Component {
+    render() {
+        return (
+            <p>Hello</p>
+        )
+    }
+}
+
+export default SongList;
--- src/containers/SongContainer.js
+++ src/containers/SongContainer.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import SongList from '../components/SongList';
 
 class SongContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            songs: [],
+            currentSong: null
+        }
+
+    }
+
+    componentDidMount() {
+        const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+        fetch(url)
+        .then(res => res.json())
+        .then(songs => this.setState({ songs: songs}))
+        .catch(err => console.error);
+    }
+
+
     render() {
         return (
-            <div>
-            <h1>Hello</h1>
+        <div>
+            <h1>Choose Your Song</h1>
+            <SongList songs={this.state.songs}></SongList>
         </div>
 
         )
